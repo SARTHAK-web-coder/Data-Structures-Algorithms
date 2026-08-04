cb9b55b38485c2c4eb0b8079ecaa71e712aad386
Sarthak-web-coder <YOUR_NOREPLY_EMAIL>
2026-08-04 22:45:35 +0530

    leet(540). Single Element in a Sorted Array

---
+++ Array Topics/BinarySearch/b5.java
@@ -0,0 +1,43 @@
+// You are given a sorted array consisting of only integers where every element
+// appears exactly twice, except for one element which appears exactly once.
+
+// Return the single element that appears only once.
+
+// Your solution must run in O(log n) time and O(1) space.
+
+// Example 1:
+
+// Input: nums = [1,1,2,3,3,4,4,8,8]
+// Output: 2
+// Example 2:
+
+// Input: nums = [3,3,7,7,10,11,11]
+// Output: 10
+/**
+ * b5
+ */
+public class b5 {
+    // 540. Single Element in a Sorted Array
+    public static void main(String[] args) {
+        int[] nums = { 1, 1, 2, 3, 3, 4, 4, 8, 8 };
+        System.out.println(singleNonDuplicate(nums));
+    }
+
+    public static int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (nums[mid] == nums[mid + 1]) {
+                left = mid + 2;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
