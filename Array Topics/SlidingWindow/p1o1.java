d38b505031d7dbf4495cf4d1aafd19d07ca6ac2a
Sarthak-web-coder <YOUR_NOREPLY_EMAIL>
2026-08-14 23:09:46 +0530

    (leet 1876) Substrings of Size Three with Distinct Characters

---
+++ Array Topics/SlidingWindow/p1o1.java
@@ -0,0 +1,39 @@
+public class p1o1 {
+    // A string is good if there are no repeated characters.
+
+    // Given a string s​​​​​, return the number of good substrings of length three
+    // in s​​​​​​.
+
+    // Note that if there are multiple occurrences of the same substring, every
+    // occurrence should be counted.
+
+    // A substring is a contiguous sequence of characters in a string.
+
+    // Example 1:
+
+    // Input: s = "xyzzaz"
+    // Output: 1
+    // Explanation: There are 4 substrings of size 3: "xyz", "yzz", "zza", and
+    // "zaz".
+    // The only good substring of length 3 is "xyz".
+
+    public static void main(String[] args) {
+        String s = "xyzzaz";
+        System.out.println(countGoodSubstrings(s));
+    }
+
+    public static int countGoodSubstrings(String s) {
+        // 1876. Substrings of Size Three with Distinct Characters
+        int n = s.length();
+        int count = 0;
+        for (int i = 0; i <= n - 3; i++) {
+            char a = s.charAt(i);
+            char b = s.charAt(i + 1);
+            char c = s.charAt(i + 2);
+            if (a != b && b != c && a != c) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
