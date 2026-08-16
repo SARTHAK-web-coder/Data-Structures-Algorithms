5645d08e39c14dcd6fe230f724cb698966ef5870
Sarthak-web-coder <YOUR_NOREPLY_EMAIL>
2026-08-16 17:03:26 +0530

    Insertion sort

--- Sorting_Method/InsertionSort.java
+++ Sorting_Method/InsertionSort.java
@@ -1,7 +1,24 @@
 package Sorting_Method;
 
+import java.util.Arrays;
+
 public class InsertionSort {
     public static void main(String[] args) {
-        
+        int arr[] = { 7, 8, 3, 1, 2 };
+        for (int i = 0; i < arr.length - 1; i++) {
+            int j = i + 1;
+            while (j > 0) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                } else {
+                    break;
+                }
+                j--;
+            }
+            // Sorting until ith index
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
